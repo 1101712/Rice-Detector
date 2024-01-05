d2a38174ed20ff0d35df368004a0ae3e6d399997
1101712 1101712 <nataliyagergo@gmail.com>
2024-01-05 14:12:10 +0000

    Enrich ml_performance page with charts and explanations: - Integrate labeled bar, pie, and color distribution charts with brief descriptions. - Enhance code in notebooks to ensure visualizations are saved.

--- app_pages/hypothesis.py
+++ app_pages/hypothesis.py
@@ -1,5 +1,73 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import joblib
+
+# Loading the model evaluation results
+evaluation = joblib.load("outputs/v2/evaluation.pkl")
 
 def hypothesis():
     st.title("Project Hypothesis")
+
+    st.write("### Hypothesis 1 and Validation")
+
+    st.success(
+        "Distinct distinguishing features can be identified in images of five rice varieties: Arborio, Basmati, Ipsala, Jasmine, and Karacadag."
+    )
+    st.info(
+        "The study involved examining images of different rice varieties provided by the client. "
+        "Each variety has unique characteristics in terms of shape, size, and texture."
+    )
+    st.write("To explore these differences, visit the Rice Varieties Visualizer tab.")
+
+    st.warning(
+        "The model was trained to recognize these distinct features, enabling it to differentiate between the rice varieties accurately."
+        " This demonstrates the model's ability to learn and generalize based on the distinguishing characteristics of each variety."
+    )
+
+    st.write("### Hypothesis 2 and Validation")
+
+    st.success(
+        "High accuracy in classifying rice varieties can be achieved with a large dataset and deep learning."
+    )
+    st.info(
+        "The project's model was trained on a dataset comprising approximately 15,000 images for each rice variety. "
+        "This extensive dataset, combined with deep learning techniques, aimed to achieve high classification accuracy."
+    )
+    st.warning(
+        "The final model achieved an accuracy of 99.30%, validating the hypothesis. "
+        "This underscores the effectiveness of using a large dataset and deep learning for precise image classification."
+    )
+
+    # Visualization of Confusion Matrix
+    st.write("### Confusion Matrix")
+    st.write("This graph shows how often the model correctly or incorrectly classifies each class of rice.")
+    st.write("The Y-axis (Actual) displays the true class labels, while the X-axis (Predicted) shows the model's predictions.")
+    st.write("Diagonal values represent the number of correct predictions the model makes for each class, while non-diagonal values show instances of misclassification.")
+    confusion_matrix_img = plt.imread("outputs/v2/performance/confusion_matrix.png")
+    st.image(confusion_matrix_img, caption='Confusion Matrix')
+
+    # Visualization of Classification Report
+    st.write("### Classification Report")
+    st.write("This graph presents key classification metrics for each class, including precision, recall, and F1-score.")
+    st.write("Precision indicates the percentage of predictions for a class that were correct.")
+    st.write("Recall reflects the percentage of actual examples of a class that were correctly identified.")
+    st.write("F1-score is the harmonic mean of precision and recall, providing an overall measure of classification accuracy.")
+    classification_report_img = plt.imread("outputs/v2/performance/classification_report.png")
+    st.image(classification_report_img, caption='Classification Report')
+
+    # Visualization of ROC Curve
+    st.write("### ROC Curve")
+    st.write("This graph represents the Receiver Operating Characteristic (ROC) curves for each class.")
+    st.write("The X-axis shows the False Positive Rate, while the Y-axis shows the True Positive Rate.")
+    st.write("An ideal model aspires to a point in the top-left corner, where the True Positive Rate is 1 and the False Positive Rate is 0.")
+    st.write("The Area Under the Curve (AUC) serves as a measure of the model's overall ability to distinguish between classes, where 1.0 is the perfect score.")
+    roc_curve_img = plt.imread("outputs/v2/performance/roc_curve.png")
+    st.image(roc_curve_img, caption='ROC Curve for Rice Varieties')
+
+    st.write(
+        "For additional information, please visit and **read** the "
+        "[Project README file](https://github.com/1101712/Rice-Detector)."
+    )
+
+# Call the function to display the hypothesis page
+hypothesis()
--- app_pages/ml_performance.py
+++ app_pages/ml_performance.py
@@ -1,7 +1,71 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import pandas as pd
-from matplotlib.image import imread
+import os
 
 def ml_performance():
-    st.title("ML Performance Metrics")
+    st.title("Machine Learning Performance Metrics")
+    
+    st.markdown("""
+        This section of the dashboard presents detailed performance metrics of the machine learning model used to classify rice varieties. It includes the distribution of image labels across the training, validation, and test sets, as well as a summary of the model's predictive accuracy. For visualizations of the Confusion Matrix, Classification Report, and ROC Curve, please refer to the _Project Hypothesis_ page.
+        """)
+
+    # Dataset Distribution
+    st.write("### Dataset Percentage Distribution")
+    st.markdown("""
+    The pie chart breaks down the dataset into training, validation, and test sets by percentage, showing us the proportion of images used for each phase of the model's development.
+
+    Train set (70% of the whole dataset) is the initial data used to 'fit' the model which will learn on this set how to generalize and make prediction on new unseen data.
+
+    Validation set (15% of the dataset) helps to improve the model performance by fine-tuning the model after each epoch (one complete pass of the training set through the model).
+
+    The test set (15% of the dataset) informs us about the final accuracy of the model after completing the training phase. It's a batch of data the model has never seen.
+    """)
+    set_distribution_path = os.path.join('outputs', 'v2', 'performance', 'set_distribution.png')
+    if os.path.exists(set_distribution_path):
+        st.image(set_distribution_path, caption='Set Distribution in Dataset')
+
+     # Label Frequencies
+    st.write("### Label Frequencies for Train, Validation, and Test Sets")
+    st.markdown("""
+    This bar chart shows the count of images for each rice variety within the training, validation, and test sets, helping us to ensure that the model is trained, validated, and tested on a balanced number of images from each category.
+    """)
+    label_distribution_path = os.path.join('outputs', 'v2', 'performance', 'label_distribution.png')
+    if os.path.exists(label_distribution_path):
+        st.image(label_distribution_path, caption='Label Distribution in Each Set')
+
+    # Color Distribution
+    st.write("### Color Distribution Across Varieties")
+    st.markdown("""
+    These histograms illustrate the average distribution of red, green, and blue color intensities across all rice images, potentially highlighting unique color features of different rice varieties.
+    """)
+    color_distribution_path = os.path.join('outputs', 'v2', 'performance', 'color_distribution.png')
+    if os.path.exists(color_distribution_path):
+        st.image(color_distribution_path, caption='Color Distribution')
+
+    # Model Accuracy
+    st.write("### Model Accuracy Over Epochs")
+    st.markdown("""
+    This line chart tracks the model's accuracy and loss at each epoch during training. A declining loss and rising accuracy indicate the model's improving performance over time.
+    Loss represents the cumulative mistakes the model makes for each example during the training (referred to as 'loss') and validation phases (referred to as 'val_loss').
+
+The value of loss indicates the model's performance after every optimization cycle — a lower loss suggests better performance.
+
+Accuracy measures the precision of the model's predictions ('accuracy') against the actual outcomes ('val_accuracy').
+
+A well-performing model on new, unseen data demonstrates its ability to generalize, indicating that it has learned the underlying patterns rather than just memorizing the training data.
+    """)
+    model_accuracy_chart_path = os.path.join('outputs', 'v2', 'performance', 'combined_loss_accuracy_plot.png')
+    if os.path.exists(model_accuracy_chart_path):
+        st.image(model_accuracy_chart_path, caption='Model Accuracy Chart')
+
+    st.markdown("""
+        ### Additional Visualizations
+        For further details on the model's performance and visual assessments such as the Confusion Matrix, Classification Report, and ROC Curve, please visit the **Project Hypothesis** page within this dashboard.
+        """)
+
+    st.markdown("""
+        ### More Information
+        For a comprehensive understanding of the project, including the methodology, dataset, and conclusions, please refer to the [Project README file](https://github.com/1101712/Rice-Detector).
+        """)
+
+# Call the function to display the performance metrics page
+ml_performance()
